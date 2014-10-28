7a452f0fa24922f4f061bd51770c2c6a73c8564f
Victor Quinn <victor@socialradar.com>
2014-10-28 11:30:25 -0400

    Initial commit

---
+++ uuid.go
@@ -0,0 +1,16 @@
+package uuid
+
+import (
+	"fmt"
+	"os"
+)
+
+func Generate() string {
+	f, _ := os.OpenFile("/dev/urandom", os.O_RDONLY, 0)
+	b := make([]byte, 16)
+	f.Read(b)
+	f.Close()
+	uuid := fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
+
+	return uuid
+}
